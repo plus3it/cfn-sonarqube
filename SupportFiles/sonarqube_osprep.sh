9757ddaeecb925306f7f56adb105075873e5061e
Thomas H Jones II <github@xanthia.com>
2017-09-08 10:53:20 -0400

    # This is a combination of 2 commits. # This is the 1st commit message:
    Nuke out EFS/gluster handlers
    
    # This is the commit message #:
    
    Nuke out EFS-related parms

--- SupportFiles/sonarqube_osprep.sh
+++ SupportFiles/sonarqube_osprep.sh
@@ -43,36 +43,6 @@ function err_exit {
    fi
 }
 
-# Ensure persistent data storage is valid
-function ValidShare {
-   SHARESRVR="${SHAREURI/\:*/}"
-   SHAREPATH=${SHAREURI/${SHARESRVR}\:\//}
-
-   echo "Attempting to validate share-path"
-   printf "\t- Attempting to mount %s... " "${SHARESRVR}"
-   if [[ ${SHARETYPE} = glusterfs ]]
-   then
-      mount -t "${SHARETYPE}" "${SHARESRVR}":/"${SHAREPATH}" /mnt && echo "Success" ||
-        err_exit "Failed to mount ${SHARESRVR}"
-   elif [[ ${SHARETYPE} = nfs ]]
-   then
-      mount -t "${SHARETYPE}" "${SHARESRVR}":/ /mnt && echo "Success" ||
-        err_exit "Failed to mount ${SHARESRVR}"
-      printf "\t- Looking for %s in %s... " "${SHAREPATH}" "${SHARESRVR}"
-      if [[ -d /mnt/${SHAREPATH} ]]
-      then
-         echo "Success"
-      else
-         echo "Not found."
-         printf "Attempting to create %s in %s... " "${SHAREPATH}" "${SHARESRVR}"
-         mkdir /mnt/"${SHAREPATH}" && echo "Success" ||
-           err_exit "Failed to create ${SHAREPATH} in ${SHARESRVR}"
-      fi
-   fi
-
-   printf "Cleaning up... "
-   umount /mnt && echo "Success" || echo "Failed"
-}
 
 ##
 ## Install any missing RPMs
@@ -104,56 +74,7 @@ function InstMissingRPM {
 }
 
 ##
-## Install any missing RPMs
-function InstMissingRPM {
-   local INSTRPMS=()
-
-   # Check if we're missing any RPMs
-   for RPM in "${RPMDEPLST[@]}"
-   do
-      printf "Cheking for presence of %s... " "${RPM}"
-      if [[ $(rpm --quiet -q "$RPM")$? -eq 0 ]]
-      then
-         echo "Already installed."
-      else
-         echo "Selecting for install"
-         INSTRPMS+=(${RPM})
-      fi
-   done
-
-   # Install any missing RPMs
-    if [[ ${#INSTRPMS[@]} -ne 0 ]]
-   then
-      echo "Will attempt to install the following RPMS: ${INSTRPMS[*]}"
-      yum install -y "${INSTRPMS[@]}" || \
-         err_exit "Install of RPM-dependencies experienced failures"
-   else
-      echo "No RPM-dependencies to satisfy"
-   fi
-}
-
-##
-## Enable NFS-client pieces
-function NfsClientStart {
-   local NFSCLIENTSVCS=(
-            rpcbind
-            nfs-server
-            nfs-lock
-            nfs-idmap
-         )
-
-    # Enable and start services
-    for SVC in "${NFSCLIENTSVCS[@]}"
-    do
-       printf "Enabling %s... " "${SVC}"
-       systemctl enable "${SVC}" && echo "Success!" || \
-          err_exit "Failed to enable ${SVC}"
-       printf "Starting %s... " "${SVC}"
-       systemctl start "${SVC}" && echo "Success!" || \
-          err_exit "Failed to start ${SVC}"
-    done
-}
-
+## Configure firewalld
 function FwSetup {
    local SELMODE=$(getenforce)
 
@@ -194,66 +115,11 @@ function FwSetup {
 }
 
 
-
 ####
 ## Main
 ####
 
-# Call firewall setup tasks
+# Call setup functions
 FwSetup
-
-# Modify some behaviors depending on Sonarqube-home's share-type
-case "${SHARETYPE}" in
-   UNDEF)
-      ;;
-   nfs)
-      RPMDEPLST+=(
-            nfs-utils
-            nfs4-acl-tools
-         )
-      (
-       printf "%s\t%s\tnfs4\t" "${SHAREURI}" "${SONARROOTDIR}" ;
-       printf "rw,relatime,vers=4.1,rsize=1048576,wsize=1048576," ;
-       printf "namlen=255,hard,proto=tcp,timeo=600,retrans=2\t0 0\n"
-      ) >> /etc/fstab || err_exit "Failed to add NFS volume to fstab"
-      ;;
-   glusterfs)
-      RPMDEPLST+=(
-            glusterfs
-            glusterfs-fuse
-            attr
-         )
-      (
-       printf "%s\t%s\tglusterfs\t" "${SHAREURI}" "${SONARROOTDIR}" ;
-       printf "defaults\t0 0\n"
-      ) >> /etc/fstab || err_exit "Failed to add NFS volume to fstab"
-      ;;
-esac
-
-# Call Setup functions
 InstMissingRPM
 
-# Start NFS Client services as necessary
-if [[ $(rpm --quiet -q nfs-utils)$? -eq 0 ]]
-then
-   NfsClientStart
-fi
-
-# Mount persistent Sonarqube home directory
-if [[ -d ${SONARROOTDIR} ]]
-then
-   echo "${SONARROOTDIR} already exists: skipping create"
-else
-   printf "Attempting to create %s... " "${SONARROOTDIR}"
-   mkdir "${SONARROOTDIR}" && echo "Success!" || \
-      err_exit "Failed to create Sonarqube root-dir"
-fi
-
-printf "Mounting %s... " "${SONARROOTDIR}"
-mount "${SONARROOTDIR}" && echo "Done." || \
-  err_exit "Failed to mount ${SONARROOTDIR}"
-
-printf "Ensuring %s is writeable by %s... " "${SONARROOTDIR}" "${SONARUSER}"
-chmod 001777 "${SONARROOTDIR}" && \
-  echo "Done." || \
-  err_exit "Failed setting perms on ${SONARROOTDIR}"
