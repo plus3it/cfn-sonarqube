ba3a6f510f047a580351c023546aa0ac6f9ea114
john.d.murphy <john.murphy@plus3it.com>
2018-09-23 17:30:14 -0400

    addressing comments to the parent-auto.groovy pipeline file

--- Deployment/Jenkins/parent-autoscale.groovy
+++ Deployment/Jenkins/parent-autoscale.groovy
@@ -29,29 +29,33 @@ pipeline {
         string(name: 'GitProjBranch', description: 'Project-branch to use from the Jenkins git project')
         string(name: 'CfnStackRoot', description: 'Unique token to prepend to all stack-element names')
         string(name: 'TemplateUrl', description: 'S3-hosted URL for the EC2 template file')
-        string(name: 'AdminPubkeyURL', description: 'URL the file containing the admin users SSH public keys')
+        string(name: 'BucketTemplate', description: 'S3-hosted URL for the template that creates SonarQubes S3 bucket(s)')
+        string(name: 'Ec2Template', description: 'S3-hosted URL for the template that creates the EC2 instance to host the SonarQube application')
+        string(name: 'EfsTemplate', description: 'S3-hosted URL for the template that creates the EFS service to host SonarQubes persistent data')
+        string(name: 'ElbTemplate', description: 'S3-hosted URL for the template that creates the ELB providing public-to-private reverse-proxy service')
+        string(name: 'IamTemplate', description: 'S3-hosted URL for the template that creates SonarQubes IAM role(s)')
+        string(name: 'RdsTemplateUri', description: 'S3-hosted URL for the template that creates SonarQubes RDS database')
+        string(name: 'SgTemplateUri', description: 'S3-hosted URL for the template that creates SonarQubes security groups')
+        string(name: 'SonarAppSetupUrl', description: 'S3-hosted URL of the Sonarqube setup script')
+        string(name: 'SonarOsPrepUrl', description: 'S3-hosted URL of the script ready the OS for Sonarqube installation')
+        string(name: 'AdminPubkeyURL', description: 'S3-hosted URL the file containing the admin users SSH public keys')
         string(name: 'AmiId', description: 'ID of the AMI to launch')
         string(name: 'BackendTimeout', defaultValue: '600', description: 'How long - in seconds - back-end connection may be idle before attempting session-cleanup')
         string(name: 'BucketInventoryTracking', defaultValue: 'false', description: '(Optional) Whether to enable generic bucket inventory-tracking. Requires setting of the ReportingBucket parameter')
-        string(name: 'BucketTemplate', description: 'URI for the template that creates SonarQubes S3 bucket(s)')
-        string(name: 'DbAdminName', description: 'Name of the SonarQube master database-user')
-        string(name: 'DbAdminPass', description: 'Password of the SonarQube master database-user')
+        string(name: 'DbAdminName', description: 'Name of the SonarQube master database-user - if rebuilding from a snapsnhot')
+        string(name: 'DbAdminPass', description: 'Password of the SonarQube master database-user - if rebuilding from a snapsnhot')
         string(name: 'DbDataSize', defaultValue: '5', description: 'Size in GiB of the RDS table-space to create')
-        string(name: 'DbInstanceName', description: 'Instance-name of the SonarQube database')
-        string(name: 'DbInstanceType', defaultValue: 'db.t2.small', description: 'Amazon RDS instance type')
+        string(name: 'DbInstanceName', description: 'Instance-name of the SonarQube database if not null then must match an existing sonar database name and have appropriate creds')
+        string(name: 'DbInstanceType', defaultValue: 'db.m4.large', description: 'Amazon RDS instance type')
         string(name: 'DbNodeName', description: 'NodeName of the SonarQube database')
         string(name: 'DbSnapshotId', defaultValue: '', description: '(Optional) RDS snapshot-ARN to clone new database from')
         string(name: 'DesiredCapacity', defaultValue: '1', description: 'Desired number of instances in the Autoscaling Group')
         string(name: 'Domainname', description: 'Suffix for Sonarqubes hostname and DNS record')
-        string(name: 'Ec2Template', description: 'URI for the template that creates the EC2 instance to host the SonarQube application')
-        string(name: 'EfsTemplate', description: 'URI for the template that creates the EFS service to host SonarQubes persistent data')
         string(name: 'ElbSubnets', description: 'Public-facing subnets for ELB to proxy requests from')
-        string(name: 'ElbTemplate', description: 'URI for the template that creates the ELB providing public-to-private reverse-proxy service')
         string(name: 'EpelRepo', defaultValue: 'epel', description: 'Name of networks EPEL repo')
         string(name: 'FinalExpirationDays', defaultValue: '30', description: 'Number of days to retain objects before aging them out of the bucket')
         string(name: 'HaSubnets', description: 'Select three subnets - each from different Availability Zones')
         string(name: 'Hostname', defaultValue: 'sonarqube', description: 'Node-name for Sonarqubes hostname and DNS record')
-        string(name: 'IamTemplate', description: 'URI for the template that creates SonarQubes IAM role(s)')
         string(name: 'InstanceType', defaultValue: 't2.large', description: 'Amazon EC2 instance type')
         string(name: 'KeyPairName', description: 'Public/private key pairs allowing the provisioning-user to securely connect to the instance after it launches')
         string(name: 'LdapAuthType', defaultValue: 'simple', description: 'Authentication-type to use with directory-service')
@@ -71,13 +75,9 @@ pipeline {
         string(name: 'ProvisionUser', defaultValue: 'builder', description: 'Default login user account name')
         string(name: 'ProxyPrettyName', description: 'A short, human-friendly label to assign to the ELB (no capital letters)')
         string(name: 'PyStache', defaultValue: 'pystache', description: 'Name of preferred pystache RPM')
-        string(name: 'RdsTemplateUri', description: 'URI for the template that creates SonarQubes RDS database')
         string(name: 'ReportingBucket', defaultValue: '', description: '(Optional) Destination for storing analytics data. Must be provided in ARN format')
         string(name: 'RolePrefix', description: 'Prefix to apply to IAM role to make things a bit prettier (optional)')
         string(name: 'ServiceTld', defaultValue: 'amazonaws.com', description: 'TLD of the created EFS-endpoint')
-        string(name: 'SgTemplateUri', description: 'URI for the template that creates SonarQubes security groups')
-        string(name: 'SonarAppSetupUrl', description: 'URL of the Sonarqube setup script')
-        string(name: 'SonarOsPrepUrl', description: 'URL of the script ready the OS for Sonarqube installation')
         string(name: 'SonarqubeRpmName', defaultValue: 'sonar', description: 'Name of Sonarqube RPM to install. Include release version if other-than-latest is desired. Example values would be: sonar, sonar-X.Y or sonar-X.Y.Z')
         string(name: 'SonarqubeBackupBucket', description: 'S3 Bucket to host Sonarqube content (Optional)')
         string(name: 'SonarqubeListenerCert', defaultValue: '', description: 'Name/ID of the ACM-managed SSL Certificate to protect public listener')
@@ -97,7 +97,7 @@ pipeline {
                 git branch: "${GitProjBranch}",
                     credentialsId: "${GitCred}",
                     url: "${GitProjUrl}"
-                writeFile file: 'sonar.auto.parms.json',
+                writeFile file: 'sonar.parent.auto.parms.json',
                     text: /
                     [
                       {
@@ -370,7 +370,7 @@ pipeline {
                         aws --region "${AwsRegion}" cloudformation create-stack --stack-name "${CfnStackRoot}-ParAuto-${BUILD_NUMBER}" \
                           --disable-rollback --capabilities CAPABILITY_NAMED_IAM \
                           --template-url "${TemplateUrl}" \
-                          --parameters file://sonar.auto.parms.json
+                          --parameters file://sonar.parent.auto.parms.json
 
                         aws cloudformation wait stack-create-complete --stack-name ${CfnStackRoot}-ParAuto-${BUILD_NUMBER} --region ${AwsRegion}
                     '''
