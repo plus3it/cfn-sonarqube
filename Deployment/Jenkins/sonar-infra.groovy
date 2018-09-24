da79540a7f1d6d74f31f36b7eeeef6686e2164c0
Thomas H Jones II <ferricoxide@users.noreply.github.com>
2018-09-24 11:32:26 +0000

    Remove extraneous sshUserPrivateKey fetch

--- Deployment/Jenkins/sonar-infra.groovy
+++ Deployment/Jenkins/sonar-infra.groovy
@@ -122,8 +122,7 @@ pipeline {
             steps {
                 withCredentials(
                     [
-                        [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${AwsCred}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'],
-                        sshUserPrivateKey(credentialsId: "${GitCred}", keyFileVariable: 'SSH_KEY_FILE', passphraseVariable: 'SSH_KEY_PASS', usernameVariable: 'SSH_KEY_USER')
+                        [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${AwsCred}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']
                     ]
                 ) {
                     sh '''#!/bin/bash
@@ -139,8 +138,7 @@ pipeline {
             steps {
                 withCredentials(
                     [
-                        [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${AwsCred}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'],
-                        sshUserPrivateKey(credentialsId: "${GitCred}", keyFileVariable: 'SSH_KEY_FILE', passphraseVariable: 'SSH_KEY_PASS', usernameVariable: 'SSH_KEY_USER')
+                        [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${AwsCred}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']
                     ]
                 ) {
                     sh '''#!/bin/bash
